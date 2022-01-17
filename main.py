c4e85af98835b1be289484618c5d573385a7965c
marksikaundi <emmanuelmark117@gmail.com>
2022-01-17 15:06:27 +0200

    pydantic basemodel

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from importlib.resources import contents
 from fastapi.params import Body
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class Post(BaseModel):
+    title: str
+    content: str
 
 @app.get("/")
 def root():
@@ -15,10 +21,17 @@ def  get_posts():
 
 # working with post methods
 
+# @app.post("/createposts")
+# def create_posts(payload: dict = Body(...)):
+#     print(payload)
+#     return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
+
+# PYDANTIC basemodel
+# title str, content str
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
+def create_posts(new_post: Post):
+    print(new_post)
+    return {"data": "new post"}
 
 
     
